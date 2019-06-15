a22a543ffe707962a3be2f4d56441a0cd34d729a
Jonelle <jonelle@starberry.us>
2019-06-15 12:14:06 -0400

    all tested successfully

--- src/main/java/com/prs/web/PurchaseRequestLineItemController.java
+++ src/main/java/com/prs/web/PurchaseRequestLineItemController.java
@@ -11,14 +11,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prs.business.JsonResponse;
 import com.prs.business.Product;
 import com.prs.business.PurchaseRequest;
 import com.prs.business.PurchaseRequestLineItem;
-import com.prs.business.User;
 import com.prs.db.PurchaseRequestLineItemRepository;
 import com.prs.db.PurchaseRequestRepository;
 import com.prs.db.ProductRepository;
@@ -77,9 +75,9 @@ public class PurchaseRequestLineItemController {
 		JsonResponse jr = null;
 		try {
 			if (purchaseRequestLineItemRepo.existsById(u.getId())) {
-			jr = JsonResponse.getInstance(purchaseRequestLineItemRepo.save(u));
-			recalculatePRTotal(u);
-		}
+				jr = JsonResponse.getInstance(purchaseRequestLineItemRepo.save(u));
+				recalculatePRTotal(u);
+			}
 			else {
 				jr=JsonResponse.getInstance("PurchaseRequest id:  "+u.getId()+"does not exist and you are attemping to modify it.");
 			}
@@ -125,88 +123,88 @@ public class PurchaseRequestLineItemController {
 		return jr;
 	}
 
-		@PostMapping("/purchase-request-line-items")
-		public JsonResponse addPRLI(@RequestBody PurchaseRequestLineItem u) {
-			JsonResponse jr = null;
-			try {
-				
-				jr=JsonResponse.getInstance(purchaseRequestLineItemRepo.save(u));
-				PurchaseRequest pr = u.getPurchaseRequest();
-				int q = u.getQuantity();
-				Product p = u.getProduct();
-				double t = pr.getTotal();
-				double itemp = p.getPrice();			
-				Double newT = t+(q*itemp);  
-				pr.setTotal(newT);
-				purchaseRequestRepo.save(pr);
-				jr=JsonResponse.getInstance("Line Item was added and the total $" + newT + " was updated.");
-			}
-			catch (Exception e ) {
-				jr=JsonResponse.getInstance(e);
-			}
-			return jr;
+	@PostMapping("/purchase-request-line-items")
+	public JsonResponse addPRLI(@RequestBody PurchaseRequestLineItem u) {
+		JsonResponse jr = null;
+		try {
+
+			jr=JsonResponse.getInstance(purchaseRequestLineItemRepo.save(u));
+			PurchaseRequest pr = u.getPurchaseRequest();
+			int q = u.getQuantity();
+			Product p = u.getProduct();
+			double t = pr.getTotal();
+			double itemp = p.getPrice();			
+			Double newT = t+(q*itemp);  
+			pr.setTotal(newT);
+			purchaseRequestRepo.save(pr);
+			jr=JsonResponse.getInstance("Line Item was added and the total $" + newT + " was updated.");
+		}
+		catch (Exception e ) {
+			jr=JsonResponse.getInstance(e);
+		}
+		return jr;
+	}
+
+	@DeleteMapping("/purchase-request-line-items/{id}")
+	public JsonResponse deletePRLI(@RequestBody PurchaseRequestLineItem u) {
+		JsonResponse jr = null;
+		try {
+			// 1st step get parent purchase request 
+			PurchaseRequest pr = u.getPurchaseRequest();
+			double oldT = pr.getTotal();
+			// 3rd step get product
+			Product p = u.getProduct();
+			double itemP = p.getPrice();
+			// 5th step calculate new total
+			int q = u.getQuantity();
+			double newT = oldT - (q * itemP  );
+			// 6th step delete prli
+
+			purchaseRequestLineItemRepo.delete(u);
+			// 7th step update pr
+			pr.setTotal(newT);
+			purchaseRequestRepo.save(pr);
+			jr=JsonResponse.getInstance("Line Item was deleted and the total $" + newT + " was updated.");
+
 		}
 
-		@DeleteMapping("/purchase-request-line-items/{id}")
-		public JsonResponse deletePRLI(@RequestBody PurchaseRequestLineItem u) {
-			JsonResponse jr = null;
-			try {
-				// 1st step get parent purchase request 
-				PurchaseRequest pr = u.getPurchaseRequest();
-				double oldT = pr.getTotal();
-				// 3rd step get product
-				Product p = u.getProduct();
-				double itemP = p.getPrice();
-				// 5th step calculate new total
-				int q = u.getQuantity();
-				double newT = oldT - (q * itemP  );
-				// 6th step delete prli
-	
-				purchaseRequestLineItemRepo.delete(u);
-				// 7th step update pr
-				pr.setTotal(newT);
-				purchaseRequestRepo.save(pr);
-				jr=JsonResponse.getInstance("Line Item was deleted and the total $" + newT + " was updated.");
-	
-			}
-	
-			catch (Exception e ) {
-				jr=JsonResponse.getInstance(e);
-			}
-			return jr;
+		catch (Exception e ) {
+			jr=JsonResponse.getInstance(e);
 		}
+		return jr;
+	}
 
-		@PutMapping("/purchase-request-line-items/{id}")
-		public JsonResponse changePRLIQ(@RequestBody PurchaseRequestLineItem u) {
-			JsonResponse jr = null;
-			try {
-				// step 5 get old PRLI
-				Optional<PurchaseRequestLineItem> optional_oldPRLI = purchaseRequestLineItemRepo.findById(u.getId());
-				PurchaseRequestLineItem oldPRLI = optional_oldPRLI.get();
-				// 1st step get PRLIQ 
-				int newQ = u.getQuantity();
-				// 2nd step get PRid
-				PurchaseRequest pr = u.getPurchaseRequest();
-				// 3rd step get total
-				double t = pr.getTotal();
-				// 4th step get product
-				Product p = u.getProduct();
-				double itemP = p.getPrice();
-				// 6th step setQuantity
-				purchaseRequestLineItemRepo.save(u);
-				// 7th step calculate new total
-				double newT = pr.getTotal() - (itemP * oldPRLI.getQuantity()) + (itemP * newQ);
-				pr.setTotal(newT);
-				purchaseRequestRepo.save(pr);
-				jr=JsonResponse.getInstance("The quantity of line Item was changed and the total $" + newT + " was updated.");
-	
-			}
-	
-			catch (Exception e ) {
-				jr=JsonResponse.getInstance(e);
-			}
-			return jr;
-		}
+	//		@PutMapping("/purchase-request-line-items/{id}")
+	//		public JsonResponse changePRLIQ(@RequestBody PurchaseRequestLineItem u) {
+	//			JsonResponse jr = null;
+	//			try {
+	//				// step 5 get old PRLI
+	//				Optional<PurchaseRequestLineItem> optional_oldPRLI = purchaseRequestLineItemRepo.findById(u.getId());
+	//				PurchaseRequestLineItem oldPRLI = optional_oldPRLI.get();
+	//				// 1st step get PRLIQ 
+	//				int newQ = u.getQuantity();
+	//				// 2nd step get PRid
+	//				PurchaseRequest pr = u.getPurchaseRequest();
+	//				// 3rd step get total
+	//				double t = pr.getTotal();
+	//				// 4th step get product
+	//				Product p = u.getProduct();
+	//				double itemP = p.getPrice();
+	//				// 6th step setQuantity
+	//				purchaseRequestLineItemRepo.save(u);
+	//				// 7th step calculate new total
+	//				double newT = pr.getTotal() - (itemP * oldPRLI.getQuantity()) + (itemP * newQ);
+	//				pr.setTotal(newT);
+	//				purchaseRequestRepo.save(pr);
+	//				jr=JsonResponse.getInstance("The quantity of line Item was changed and the total $" + newT + " was updated.");
+	//	
+	//			}
+	//	
+	//			catch (Exception e ) {
+	//				jr=JsonResponse.getInstance(e);
+	//			}
+	//			return jr;
+	//		}
 
 	private void recalculatePRTotal(PurchaseRequestLineItem u) {
 		PurchaseRequest pr = u.getPurchaseRequest();
